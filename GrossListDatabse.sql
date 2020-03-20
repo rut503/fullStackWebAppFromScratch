93c91c767f932b7493cb60edee4c5a0186dfe538
Rutvik Patel <rutvik503@gmail.com>
2020-03-19 23:50:06 -0500

    added sql script to set up local database.

---
+++ GrossListDatabse.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA GrossListDatabase;
+
+CREATE TABLE GrossListDatabase.GrossList (
+    ID INT AUTO_INCREMENT,
+    IName VARCHAR(30) NOT NULL,
+    IPriority VARCHAR(6) NOT NULL,
+    IStore VARCHAR(15),
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO GrossListDatabase.GrossList(IName, IPriority, IStore) values("Toilet papers", "HIGH", "Walmart");
+INSERT INTO GrossListDatabase.GrossList(IName, IPriority, IStore) values("Lemon cakes", "LOW", "ALDI");
+INSERT INTO GrossListDatabase.GrossList(IName, IPriority, IStore) values("Milk", "MEDIUM", "CVS");
+INSERT INTO GrossListDatabase.GrossList(IName, IPriority, IStore) values("Soda", "LOW", "Walmart");
+INSERT INTO GrossListDatabase.GrossList(IName, IPriority, IStore) values("Bananas", "HIGH", "Valli");
+INSERT INTO GrossListDatabase.GrossList(IName, IPriority, IStore) values("Apples", "LOW", "ALDI");
+
+SELECT * FROM GrossListDatabase.GrossList;
